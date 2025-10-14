fbf0ab2c65c1eb707e24b9db5d5d1276edd8e454
Timur Kady <kadyevtr@gmail.com>
2025-10-14 17:01:06 +0300

    Add application factory for FastAPI integration

--- freeadmin/__init__.py
+++ freeadmin/__init__.py
@@ -8,11 +8,12 @@ Author: Timur Kady
 Email: timurkady@yandex.com
 """
 
+from .application import ApplicationFactory
+from .conf import FreeAdminSettings, configure, current_settings
 from .core.base import BaseModelAdmin
 from .core.site import AdminSite
 from .meta import __version__
 from .router import AdminRouter
-from .conf import FreeAdminSettings, configure, current_settings
 
 # The End
 
