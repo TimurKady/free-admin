02bb89c3b0d6bea473f6dfc820fb00dc254a1176
Timur Kady <kadyevtr@gmail.com>
2025-10-16 00:36:04 +0300

    Restore compatibility module for CRUD helpers

---
+++ freeadmin/crud.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""crud
+
+Compatibility facade exposing CRUD helpers.
+
+Version:0.1.0
+Author: Timur Kady
+Email: timurkady@yandex.com
+"""
+
+from __future__ import annotations
+
+from .contrib.crud import CrudRouterBuilder
+
+__all__ = ["CrudRouterBuilder"]
+
+
+# The End
+
